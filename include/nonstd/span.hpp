c85c2fdb246b3654e0efea027243ceba8103ff71
Martin Moene <martin.moene@gmail.com>
2018-05-25 16:03:31 +0200

    Simplify throw_out_of_range() to just use printf() to report index/size

--- include/nonstd/span.hpp
+++ include/nonstd/span.hpp
@@ -536,31 +536,21 @@ struct is_array<T[N]> : std::true_type {};
 
 // format index and size:
 
-#if span_HAVE_SNPRINTF 
-# define span_FORMAT_RANGE(b, n, fmt, idx, size)  (std::snprintf)( (b), (n), (fmt), (idx), (size) )
-#elif span_COMPILER_MSVC_VERSION
-# define span_FORMAT_RANGE(b, n, fmt, idx, size)  (::sprintf_s)( (b), (n), (fmt), (idx), (size) )
-#else
-# define span_FORMAT_RANGE(b, n, fmt, idx, size)  (::sprintf)( (b), (fmt), (idx), (size) )
-#endif
-
-#if span_HAVE_LONGLONG
-#define span_FMT_LLI "%lli"
-inline long long to_longlong( index_t x ) { return static_cast<long long>(x); }
-#else
-#define span_FMT_LLI "%li"
-inline long to_longlong( index_t x ) { return static_cast<long>(x); }
+#if defined __clang__
+# pragma clang diagnostic ignored "-Wlong-long"
+#elif defined __GNUC__
+# pragma GCC   diagnostic ignored "-Wformat=ll"
+# pragma GCC   diagnostic ignored "-Wlong-long"
 #endif
 
 inline void throw_out_of_range( index_t idx, index_t size )
 {
-    const char fmt[] = "span::at(): index '" span_FMT_LLI "' is out of range [0.." span_FMT_LLI ")";
+    const char fmt[] = "span::at(): index '%lli' is out of range [0..%lli)";
     char buffer[ 2 * 20 + sizeof fmt ];
-    span_FORMAT_RANGE( buffer, sizeof buffer, fmt, to_longlong(idx), to_longlong(size) );
+    sprintf( buffer, fmt, static_cast<long long>(idx), static_cast<long long>(size) );
     
     throw std::out_of_range( buffer );
 }
-#undef span_FMT_LLI
 
 #else // MEMBER_AT
 
