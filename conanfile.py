d462ddb4e02a30016c04bdc7cd9a7c4a9edc0492
Paul le Roux <pleroux0@outlook.com>
2019-03-18 13:49:50 +0200

    feat: Allow better conan/cmake interop
    Makes find_package(span-lite) work as expected from CMake projects that
    use conan to handle dependencies.

--- conanfile.py
+++ conanfile.py
@@ -1,4 +1,4 @@
-from conans import ConanFile
+from conans import ConanFile, CMake
 
 class SpanLiteConan(ConanFile):
     version = "0.4.0"
@@ -6,8 +6,8 @@ class SpanLiteConan(ConanFile):
     description = "A C++20-like span for C++98, C++11 and later in a single-file header-only library"
     license = "Boost Software License - Version 1.0. http://www.boost.org/LICENSE_1_0.txt"
     url = "https://github.com/martinmoene/span-lite.git"
-    exports_sources = "include/nonstd/*", "LICENSE.txt"
-    build_policy = "missing"    
+    exports_sources = "include/nonstd/*", "CMakeLists.txt", "cmake/*", "LICENSE.txt"
+    build_policy = "missing"
     author = "Martin Moene"
 
     def build(self):
@@ -15,8 +15,12 @@ class SpanLiteConan(ConanFile):
         pass
 
     def package(self):
-        """Provide pkg/include/nonstd/*.hpp"""
-        self.copy("*.hpp")
+        """Run CMake install"""
+        cmake = CMake(self)
+        cmake.definitions["SPAN_LITE_OPT_BUILD_TESTS"] = "OFF"
+        cmake.definitions["SPAN_LITE_OPT_BUILD_EXAMPLES"] = "OFF"
+        cmake.configure()
+        cmake.install()
 
     def package_info(self):
         self.info.header_only()
