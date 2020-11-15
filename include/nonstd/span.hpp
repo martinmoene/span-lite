845e0867b0354040bf7a593ac1837dff9b6937fa
Martin Moene <martin.moene@gmail.com>
2020-11-15 22:22:20 +0100

    Replace _CPPUNWIND with _HAS_EXCEPTIONS (thanks @DBJDBJ)
    See also https://github.com/martinmoene/nonstd-lite-project/issues/48

--- include/nonstd/span.hpp
+++ include/nonstd/span.hpp
@@ -119,7 +119,10 @@
 // Control presence of exception handling (try and auto discover):
 
 #ifndef span_CONFIG_NO_EXCEPTIONS
-# if defined(__cpp_exceptions) || defined(__EXCEPTIONS) || defined(_CPPUNWIND)
+# if _MSC_VER
+# include <cstddef>     // for _HAS_EXCEPTIONS
+# endif
+# if defined(__cpp_exceptions) || defined(__EXCEPTIONS) || (_HAS_EXCEPTIONS)
 #  define span_CONFIG_NO_EXCEPTIONS  0
 # else
 #  define span_CONFIG_NO_EXCEPTIONS  1
