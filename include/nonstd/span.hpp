4dde28182873896525447caa2e5b2363e8b8363c
Martin Moene <martin.moene@gmail.com>
2025-10-08 17:32:01 +0200

    For now, adapt last change to the current style, even if that wasn't a particularly good or helpful choice on my side

--- include/nonstd/span.hpp
+++ include/nonstd/span.hpp
@@ -494,7 +494,7 @@ span_DISABLE_MSVC_WARNINGS( 26439 26440 26472 26473 26481 26490 )
 
 #if ! span_CONFIG( NO_EXCEPTIONS )
 # include <stdexcept>
-#elif ! span_CONFIG_CONTRACT_VIOLATION_THROWS_V
+#elif ! span_CONFIG( CONTRACT_VIOLATION_THROWS_V )
 # include <exception>
 #endif
 
